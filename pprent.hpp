897baa97a4b9cad66107511821ba2a810550aa5c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-21 11:54:36 +0100

    iter implementation

--- pprent.hpp
+++ pprent.hpp
@@ -4,17 +4,18 @@ struct DIR;
 
 namespace pprent {
 class iter {
-  DIR *m_dir;
-  dirent *m_ent;
+  DIR *m_dir{};
+  dirent *m_ent{};
   friend class list;
 
   explicit iter(DIR *dr);
-  explicit iter();
+  explicit constexpr iter() = default;
 
 public:
-  bool operator!=(const iter &) const;
-  iter operator++();
-  const char *operator*();
+  bool operator!=(const iter &o) const;
+
+  iter &operator++();
+  const char *operator*() const;
 };
 
 class list {
@@ -42,6 +43,17 @@ list::~list() {
 iter list::begin() { return m_dir ? iter{m_dir} : iter{}; }
 iter list::end() { return iter{}; }
 
+iter::iter(DIR *dr) : m_dir{dr}, m_ent{readdir(m_dir)} {}
+bool iter::operator!=(const iter &o) const {
+  // pointer comparison suffices for "normal" usage
+  return m_dir != o.m_dir || m_ent != o.m_ent;
+}
+iter &iter::operator++() {
+  if (m_dir)
+    m_ent = readdir(m_dir);
+  return *this;
+}
+const char *iter::operator*() const { return m_ent->d_name; }
 } // namespace pprent
 
 #endif // PPRENT_IMPLEMENTATION
